67884c5e36541271f58e74f490ac7e4f7af377b3
C6H4O2 <2535771636@qq.com>
2025-03-26 21:52:40 +0800

    aaa

---
+++ 4.neuro_network_study/test3.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+# 中心差分
+def numerical_diff(f,x):
+    h = 1e-4
+    return (f(x+h) - f(x-h)) / (2*h)
+
+def func_1(x):
+    return 0.01*x**2 + 0.1*x
+
+x = np.arange(0.0,20.0,0.1)
+y = func_1(x)
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.plot(x,y)
+plt.show()
+
